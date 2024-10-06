27633b6230a8a862316885540be28cdab7d8d4d3
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-10-06 16:03:23 +0530

    cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,4 @@
+import CabinCard from "@/app/_components/CabinCard";
 
 export const metadata = {
   title: "Cabins"
@@ -5,11 +6,31 @@ export const metadata = {
 
 // NOTE: you can call this component anything you like but many people use "Page"
 const Page = () => {
+  const cabins = [];
+
   return (
     <div>
-      <h1>Cabins Page</h1>
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
+        away from home. The perfect spot for a peaceful, calm vacation. Welcome
+        to paradise.
+      </p>
+
+      {cabins.length > 0 && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Page
