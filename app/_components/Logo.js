d9115c16d25fb6fc95e728a44cb2bd82d25e2b42
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-10-06 14:18:09 +0530

    Use nextjs Image component in logo for rendering logo image

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,14 @@
+import logoImg from "@/public/logo.png";
+import Image from "next/image";
 import Link from "next/link";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+      {/* NOTE: this is really the most basic way in which you can use nextjs Image component there are other ways also in which you can use nextjs Image component. */}
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+      {/* NOTE: using src as imported image, here you can omit heigt and width for the image. */}
+      <Image src={logoImg} height={60} width={60} quality={100} alt="The Wild Oasis logo" />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
