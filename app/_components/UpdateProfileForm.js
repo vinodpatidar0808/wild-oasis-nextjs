bcb912297d4641056686d946d562633f0406a9cb
Vinod Patidar <vinodpatidar0808@gmail.com>
2024-10-19 17:14:24 +0530

    rendering server components inside client components

---
+++ app/_components/UpdateProfileForm.js
@@ -0,0 +1,68 @@
+'use client'
+
+import { useState } from "react";
+
+const UpdateProfileForm = ({ children }) => {
+  const [count, setcount] = useState()
+  const countryFlag = "pt.jpg";
+  // const nationality = "portugal";
+
+  return (
+    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <div className="space-y-2">
+        <label>Full name</label>
+        <input
+          disabled
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label>Email address</label>
+        <input
+          disabled
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <label htmlFor="nationality">Where are you from?</label>
+          <img
+            src={countryFlag}
+            alt="Country flag"
+            className="h-5 rounded-sm"
+          />
+        </div>
+
+        {/* NOTE: this is a server component being used inside client component */}
+        {/* This will throw errors as server components can not be imported inside client component. work around pass server components to child component from server components as children of client component. --> see profile page for implementation */}
+        {/* <SelectCountry
+          name="nationality"
+          id="nationality"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+          defaultCountry={nationality}
+        /> */}
+
+        {/* NOTE: rendering server component as child component */}
+        {children}
+      </div>
+
+      <div className="space-y-2">
+        <label htmlFor="nationalID">National ID number</label>
+        <input
+          name="nationalID"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+        />
+      </div>
+
+      <div className="flex justify-end items-center gap-6">
+        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+          Update profile
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default UpdateProfileForm
